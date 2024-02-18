8eb0301fa0351e01d392d6ba34d78c1c17f1dec5
Vinicius Assis <vinicius.coassis@gmail.com>
2024-02-18 18:29:00 -0300

    format

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,22 +1,25 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-pragma solidity ^0.8.18;
-
-import { Script } from "forge-std/Script.sol";
-import { BasicNft } from "../src/BasicNft.sol";
-import { DevOpsTools } from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
-    string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 deployerKey;
+
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
-        mintNftOnContract(mostRecentlyDeployed);
+        address mostRecentlyDeployedBasicNft = DevOpsTools
+            .get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
     }
 
-    function mintNftOnContract(address contractAddress) public {
+    function mintNftOnContract(address basicNftAddress) public {
         vm.startBroadcast();
-        BasicNft(contractAddress).mintNft(PUG);
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
         vm.stopBroadcast();
-
     }
 }
